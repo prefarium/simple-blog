e5f600052e8e5f109413f4012fdbe0fe3fa3678a
Anton Krestyaninov <9593795@gmail.com>
2020-07-12 13:20:21 +0300

    Add new, create and destroy actions to Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,27 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
 
   def show
     @article = Article.find params[:id]
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new params_for_new_article
+    @article.save
+    redirect_to article_path @article
+  end
+
+  def destroy
+    @article = Article.find params[:id]
+    @article.destroy
+    redirect_to articles_path
   end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,5 @@
 module ArticlesHelper
+  def params_for_new_article
+    params.require(:article).permit(:title, :body)
+  end
 end
