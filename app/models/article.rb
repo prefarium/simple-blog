81c4c00c1a5ff9254b823257ddb5400da813882c
Anton Krestyaninov <9593795@gmail.com>
2020-07-13 05:24:13 +0300

    Tags can be added when creating or editing an article but cannot be seen yet

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,16 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+  # tags = article.taggings.map { |tagging| tagging.tag }
+
+  def tag_list
+    tags.map { |tag| tag.name }.join(", ")
+  end
+
+  def tag_list=(tags_string)
+    tag_names    = tags_string.downcase.scan(/\w+/).uniq
+    article_tags = tag_names.map { |name| Tag.find_or_create_by(name: name) }
+    self.tags    = article_tags
+  end
 end
