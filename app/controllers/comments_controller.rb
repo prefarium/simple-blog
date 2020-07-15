c66440b371ffda94b08ddddbfb602c8fd910239c
Anton Krestyaninov <9593795@gmail.com>
2020-07-15 14:23:23 +0300

    Add comments that can be added but not deleted or edited

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+  include CommentsHelper
+  add_flash_types :success
+
+  def create
+    @comment            = Comment.new(comment_params)
+    @comment.article_id = params[:article_id]
+
+    @comment.save
+
+    redirect_to article_path(@comment.article), success: 'Article is created'
+  end
+end
